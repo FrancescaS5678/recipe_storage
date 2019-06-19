dbd03bcb39e806132b85dc76a09dfbddb1079879
Francesca Marie Schultz <francescamschultz@gmail.com>
2019-06-19 17:34:47 -0600

    Updated URL in calls

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -4,7 +4,7 @@ import { GET_RECIPES, ADD_RECIPE, DELETE_RECIPE, RECIPES_LOADING } from './types
 export const getRecipes = () => dispatch => {
     dispatch(setRecipesLoading());
     axios
-        .get('http://localhost:4000/api/recipes')
+        .get('https://mysterious-earth-62439.herokuapp.com//api/recipes')
         .then(res =>
             dispatch({
                 type: GET_RECIPES,
@@ -15,7 +15,7 @@ export const getRecipes = () => dispatch => {
 
 export const addRecipe = (recipe) => dispatch => {
     axios
-        .post('http://localhost:4000/api/recipes', recipe)
+        .post('https://mysterious-earth-62439.herokuapp.com/api/recipes', recipe)
         .then(res =>
             dispatch({
                 type: ADD_RECIPE,
@@ -26,7 +26,7 @@ export const addRecipe = (recipe) => dispatch => {
 
 export const deleteRecipes = (id) => dispatch => {
     axios
-        .delete(`/api/recipes/${id}`)
+        .delete(`https://mysterious-earth-62439.herokuapp.com/api/recipes/${id}`)
         .then(res =>
             dispatch({
                 type:DELETE_RECIPE,
